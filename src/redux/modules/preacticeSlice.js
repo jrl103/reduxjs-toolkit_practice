6b7b281a7d18fd61657860fa259ef95b1db8b40c
jrl103 <jrl103@naver.com>
2022-06-17 23:31:38 +0900

    리덕스 기본 코드

---
+++ src/redux/modules/preacticeSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit"; 
+
+// Reducer
+const practiceSlice = createSlice({ 
+    name: "practice", 
+    initialState: { 
+
+    }, 
+    reducers: {
+
+     }, 
+    }); 
+    
+    
+    
+const { } = practiceSlice.actions;
+ export default practiceSlice.reducer;
